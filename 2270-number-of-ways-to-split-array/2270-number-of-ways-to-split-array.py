fc73c99ca2203778617d80637c2c5fb538b5673e
Kudzaishe Chikowore <chikoworekudzaishe9@gmail.com>
2025-10-25 17:07:07 -0500

    Time: 59 ms (55.07%), Space: 32.2 MB (86.55%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.py
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.py
@@ -1,16 +1,11 @@
 class Solution:
     def waysToSplitArray(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
         count = 0
-        right_sum = 0
-        left_sum = sum(nums)
-        for i in range(0, len(nums) - 1):
-            right_sum += nums[i]
-            left_sum -= nums[i]
-            if right_sum >= left_sum:
+        left, right = 0, sum(nums)
+        for i in range(len(nums) - 1):
+            left += nums[i]
+            right -= nums[i]
+            if left >= right:
                 count += 1
         return count
-
-
-
+        
